7b1a3902dbc2f445f37898373fcd68a52704dc92
Shakil <linux.shakil@gmail.com>
2026-07-18 16:49:03 +0000

    fix: Error while deploying wordpress

--- modules/wordpress/outputs.tf
+++ modules/wordpress/outputs.tf
@@ -6,10 +6,6 @@ output "service_account" {
   value = kubernetes_service_account.wordpress.metadata[0].name
 }
 
-output "storage_class" {
-  value = kubernetes_storage_class.wordpress.metadata[0].name
-}
-
 output "persistent_volume_claim" {
   value = kubernetes_persistent_volume_claim.wordpress.metadata[0].name
 }
