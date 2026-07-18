f608cbec319fd6f66b82190a147290419ac6775f
Shakil <linux.shakil@gmail.com>
2026-07-18 17:11:22 +0000

    fix: PVC is managed by the Bitnami Helm chart.

--- modules/wordpress/outputs.tf
+++ modules/wordpress/outputs.tf
@@ -6,9 +6,6 @@ output "service_account" {
   value = kubernetes_service_account.wordpress.metadata[0].name
 }
 
-output "persistent_volume_claim" {
-  value = kubernetes_persistent_volume_claim.wordpress.metadata[0].name
-}
 
 output "helm_release" {
   value = helm_release.wordpress.name
