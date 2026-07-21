32aba3d1d5863a7b46d12e80b747e6a41631e461
linuxshakil <linux.shakil@gmail.com>
2026-07-21 20:52:21 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,64 +1,30 @@
-terraform {
-
-  required_version = ">= 1.9"
-
-  required_providers {
-
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
-    }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 6.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.38"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.17"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.7"
-    }
-  }
-}
-
 provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-provider "google-beta" {
+
   project = var.project_id
   region  = var.region
 }
 
-data "google_client_config" "default" {}
+provider "google-beta" {
+
+  project = var.project_id
+  region  = var.region
+}
 
 provider "kubernetes" {
 
-  host = "https://${module.gke.cluster_endpoint}"
-
-  cluster_ca_certificate = base64decode(module.gke.cluster_ca_cert)
-
-  token = data.google_client_config.default.access_token
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
 provider "helm" {
 
   kubernetes {
 
-    host = "https://${module.gke.cluster_endpoint}"
-
-    cluster_ca_certificate = base64decode(module.gke.cluster_ca_cert)
-
-    token = data.google_client_config.default.access_token
+    host                   = "https://${module.gke.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
   }
 }
+
+data "google_client_config" "default" {}
