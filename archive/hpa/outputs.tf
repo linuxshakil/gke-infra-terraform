bf97c837acfb79a477a3a88e70cc14611138d675
Shakil <linux.shakil@gmail.com>
2026-07-28 11:22:42 +0000

    Remove unused files

--- archive/hpa/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "hpa_name" {
-  value = kubernetes_horizontal_pod_autoscaler_v2.wordpress.metadata[0].name
-}
--- archive/jenkins/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "jenkins_namespace" {
-  value = kubernetes_namespace.cicd.metadata[0].name
-}
-
-output "jenkins_sa_email" {
-  value = google_service_account.jenkins_sa.email
-}
